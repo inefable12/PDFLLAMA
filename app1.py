1134d49f605f6819337b92bf939d7a1170e55b03
Jesus_Alvarado-Huayhuaz <inefable12@gmail.com>
2024-11-14 17:09:44 -0300

    Update app1.py

--- app1.py
+++ app1.py
@@ -7,15 +7,19 @@ from io import StringIO
 st.title("PARTE 1: Extrae texto de PDF")
 
 # Agregar un botón para cargar archivo, solo permitiendo archivos PDF
-pdf_file = st.file_uploader("Cargar archivo PDF", type="pdf")
+pdf_file_obj = st.file_uploader("Cargar archivo PDF", type="pdf")
 
 # Mostrar un mensaje si se carga el archivo correctamente
-if pdf_file is not None:
+if pdf_file_obj is not None:
     st.success("Archivo PDF cargado exitosamente")
-    pdf_file_obj = open(pdf_file, 'rb')
-    # Aquí puedes realizar operaciones adicionales con el archivo PDF
+    #pdf_file_obj = open(pdf_file, 'rb')
+    pdf_reader = PdfReader(pdf_file_obj)
+    text = ""
+    for page in pdf_reader.pages:
+        text += page.extract_text()
+    st.write(text_data)
 else:
     st.info("Por favor, sube un archivo PDF.")
 
 
-#pdf_reader = PdfReader(pdf_file_obj)
+#
